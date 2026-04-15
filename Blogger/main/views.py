059206f6103baca1f016a8e5701e99ddba1b7275
Ali Alshehri <ps4ps5cc@gmail>
2026-04-15 06:10:36 +0300

    solve lab + change TIME_ZONE

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpRequest,HttpResponse
+from .models import Post
 
 # Create your views here.
+
+ 
+def home_view(request:HttpRequest):
+    post=Post.objects.all()
+    return render(request, 'main/index.html', {'posts': post})
+
+ 
+def add_post_view(request:HttpRequest):
+    if request.method == "POST":
+        print(request.POST)
+        new_post = Post(title=request.POST["title"], content=request.POST["content"])
+        new_post.save()
+        return redirect('home_view')
+    return render(request, "main/add_post.html")
