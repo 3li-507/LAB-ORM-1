0175d1aae280f76a857f7121b7bdc14144f617a4
Ali Alshehri <ps4ps5cc@gmail>
2026-04-16 04:21:43 +0300

    add media files feature !

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -13,7 +13,7 @@ def home_view(request:HttpRequest):
 def add_post_view(request:HttpRequest):
     if request.method == "POST":
         print(request.POST)
-        new_post = Post(title=request.POST["title"], content=request.POST["content"])
+        new_post = Post(title=request.POST["title"], content=request.POST["content"],poster=request.FILES["poster"])
         new_post.save()
-        return redirect('home_view')
+        return redirect('main:home_view')
     return render(request, "main/add_post.html")
