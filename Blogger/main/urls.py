e9320bdc7e60eaf3418a2215f61f54ac22fdacac
Ali Alshehri <ps4ps5cc@gmail>
2026-04-16 07:08:05 +0300

    add update and delete features

--- Blogger/main/urls.py
+++ Blogger/main/urls.py
@@ -6,4 +6,7 @@ app_name="main"
 urlpatterns = [
     path('', views.home_view, name='home_view'),
     path('add/', views.add_post_view, name='add_post_view'),
+    path('details/<int:poster_id>/', views.detail_post_view, name='detail_post_view'),
+    path('update/<int:poster_id>/', views.update_post_view, name='update_post_view'),  
+    path('delete/<int:poster_id>/', views.delete_post_view, name='delete_post_view'),
 ]
--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -10,10 +10,48 @@ def home_view(request:HttpRequest):
     return render(request, 'main/index.html', {'posts': post})
 
  
-def add_post_view(request:HttpRequest):
+# def add_post_view(request:HttpRequest):
+#     if request.method == "POST":
+#         print(request.POST)
+#         new_post = Post(title=request.POST["title"], content=request.POST["content"],poster=request.FILES["poster"])
+#         new_post.save()
+#         return redirect('main:home_view')
+def add_post_view(request: HttpRequest):
     if request.method == "POST":
-        print(request.POST)
-        new_post = Post(title=request.POST["title"], content=request.POST["content"],poster=request.FILES["poster"])
+        new_post = Post(
+            title=request.POST["title"],
+            content=request.POST["content"],
+        )
+        if request.FILES.get("poster"):
+            new_post.poster = request.FILES["poster"]
         new_post.save()
         return redirect('main:home_view')
     return render(request, "main/add_post.html")
+    
+    return render(request, "main/add_post.html")
+
+def detail_post_view(request:HttpRequest, poster_id:int):
+    post = Post.objects.get(pk=poster_id)
+    # print(post.title)
+
+    return render(request, 'main/detail_post.html', {'post':post})
+
+def update_post_view(request: HttpRequest, poster_id: int):
+    post = Post.objects.get(pk=poster_id)
+
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        # if request.FILES.get("poster"):
+        #     post.poster = request.FILES["poster"]
+        if 'poster' in request.FILES: post.poster= request.FILES["poster"]
+        post.save()
+        return redirect('main:detail_post_view', poster_id=post.pk)
+    return render(request, 'main/update_post.html', {'post': post})
+
+
+def delete_post_view(request: HttpRequest, poster_id: int):   # أضف
+    post = Post.objects.get(pk=poster_id)
+    post.delete()
+
+    return redirect('main:home_view')
